9bb76f6db9d088f93be5bb51c2157d9ad36669ee
Colin Bruin <colinjb23@gmail.com>
2018-02-22 16:46:09 -0500

    making big deals

--- connect.js
+++ connect.js
@@ -1,9 +1,3 @@
-// var cells = document.getElementsByClassName("button-container");
-//
-// for(var i = 0; i < cells.length; i++) {
-//   cells[i].addEventListener('click', handleClick);
-// }
-
 const gridSystem = [
   [0,0,0,0,0,0],
   [0,0,0,0,0,0],
@@ -13,6 +7,8 @@ const gridSystem = [
   [0,0,0,0,0,0],
   [0,0,0,0,0,0]
 ]
+// console.log(gridSystem.length)
+
 
 function createGrid() {
   const nodeWrapper = document.getElementById('wrapper');
@@ -22,20 +18,45 @@ function createGrid() {
     rowNode.className = 'row';
 
     for (let j = 0; j < gridSystem[i].length; j++) {
-
       let columnNode = document.createElement('div');
       columnNode.className = 'column';
-      let textNode = document.createTextNode("Water");
-      columnNode.appendChild(textNode);
+      columnNode.setAttribute('id', String(i)+String(j));
       rowNode.appendChild(columnNode);
-
     }
 
     nodeWrapper.appendChild(rowNode);
   }
 }
 
+function createHandler() {
+  var cells = document.getElementsByClassName("column");
+
+  for(let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', handleClick);
+  }
+}
+
+function handleClick(event) {
+
+  // let currentCell = document.getElementById();
+  // let newElement = document.createElement('div');
+  // let nodeToAppendTo = document.getElementById(event.target.id);
+  // newElement.className = 'chip';
+  // nodeToAppendTo.appendChild(newElement);
+
+  for(let i = gridSystem.length-1; i>=0; i--) {
+    for(let j=gridSystem[i].length-1; j>=0; j--) {
+      console.log(String(j) + String(i));
+      if (String(event.target.id)[0] === i) {
+        // console.log(i);
+      }
+    }
+  }
+}
+
 createGrid();
+createHandler();
+
 
 
 // Psuedo
